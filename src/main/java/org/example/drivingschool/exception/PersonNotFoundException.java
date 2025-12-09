745502d4541a72badc573949e98c9f003bc26b70
dvoinescu <denis.voinescu@endava.com>
2025-12-09 14:36:10 +0200

    Added timestamp to "Error" in the YAML specification. Also implemented getById method in service and controller and treated InvalidIdException and ResourceNotFoundException

--- src/main/java/org/example/drivingschool/exception/PersonNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.drivingschool.exception;
-
-public class PersonNotFoundException extends RuntimeException {
-    public PersonNotFoundException(Integer id) {
-        super("Person with ID " + id + " not found.");
-    }
-}
