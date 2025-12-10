f8b122109db4389ffcb7ba7ad98cbfb00190f5c7
dvoinescu <denis.voinescu@endava.com>
2025-12-10 15:12:47 +0200

    Added vehicle API methods

--- src/main/java/org/example/drivingschool/controller/VehicleController.java
+++ src/main/java/org/example/drivingschool/controller/VehicleController.java
@@ -1,14 +1,47 @@
 package org.example.drivingschool.controller;
 
+import jakarta.validation.Valid;
 import org.example.drivingschool.service.VehicleService;
-import org.springframework.web.bind.annotation.RestController;
+import org.openapitools.api.VehiclesApi;
+import org.openapitools.model.Vehicle;
+import org.openapitools.model.VehicleCreate;
+import org.openapitools.model.VehicleUpdate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-public class VehicleController {
+@RequestMapping("api/v1/vehicles")
+public class VehicleController implements VehiclesApi {
 
     private final VehicleService vehicleService;
 
     public VehicleController(VehicleService vehicleService) {
         this.vehicleService = vehicleService;
+    }
+
+    @Override
+    @GetMapping
+    public ResponseEntity<List<Vehicle>> listVehicles() {
+        return ResponseEntity.ok(vehicleService.list());
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public ResponseEntity<Vehicle> getVehicleById(@PathVariable Integer id) {
+        return ResponseEntity.ok(vehicleService.getById(id));
+    }
+
+    @Override
+    @PostMapping
+    public ResponseEntity<Vehicle> createVehicle(@Valid @RequestBody VehicleCreate vehicleCreate) {
+        return ResponseEntity.ok(vehicleService.create(vehicleCreate));
+    }
+
+    @Override
+    @PatchMapping("/{id}")
+    public ResponseEntity<Vehicle> updateVehicle(@PathVariable Integer id, @RequestBody VehicleUpdate vehicleUpdate) {
+        return ResponseEntity.ok(vehicleService.update(id, vehicleUpdate));
     }
 }
--- src/main/java/org/example/drivingschool/mapper/VehicleMapper.java
+++ src/main/java/org/example/drivingschool/mapper/VehicleMapper.java
@@ -2,18 +2,25 @@ package org.example.drivingschool.mapper;
 
 import org.example.drivingschool.model.VehicleEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.openapitools.model.Vehicle;
 import org.openapitools.model.VehicleCreate;
+import org.openapitools.model.VehicleUpdate;
 
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
 public interface VehicleMapper {
 
     VehicleEntity toEntity(VehicleCreate dto);
     Vehicle toDto(VehicleEntity entity);
+    void updateEntity(@MappingTarget VehicleEntity entity, VehicleUpdate dto);
 
     default OffsetDateTime map(Instant value) {
         return value == null ? null : value.atOffset(ZoneOffset.UTC);
