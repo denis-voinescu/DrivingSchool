ce5898eb64ff762fa1ef5d53e7f08d07404c87eb
dvoinescu <denis.voinescu@endava.com>
2025-12-10 13:35:41 +0200

    Added get instructorById endpoint

--- src/main/java/org/example/drivingschool/controller/InstructorController.java
+++ src/main/java/org/example/drivingschool/controller/InstructorController.java
@@ -5,6 +5,7 @@ import org.openapitools.api.InstructorsApi;
 import org.openapitools.model.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,8 +28,10 @@ public class InstructorController implements InstructorsApi {
     }
 
     @Override
-    public ResponseEntity<Instructor> getInstructorById(Integer id) {
-        return null;
+    @GetMapping("/{id}")
+    public ResponseEntity<Instructor> getInstructorById(@PathVariable Integer id) {
+        return ResponseEntity.ok(instructorService.getById(id));
+
     }
 
     @Override
